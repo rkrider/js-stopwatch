4dfcdc5c06eac7bbd4b3ccd683bc3307ac40bbdc
Bumblebee <rahulomnitrics@gmail.com>
2023-09-22 02:30:01 +0530

    ms added

--- index.js
+++ index.js
@@ -1,43 +1,48 @@
-let [seconds, minutes, hours] = [0, 0, 0];
+let [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
 
 const displayTime = document.getElementById("time");
 const playPauseBtn = document.getElementById("play-pause");
 let timer = null;
 
 function stopwatch() {
-  seconds++;
-  if (seconds === 60) {
-    seconds = 0;
-    minutes++;
-    if (minutes === 60) {
-      minutes = 0;
-      hours++;
+  milliseconds++;
+  if (milliseconds === 100) {
+    milliseconds = 0;
+    seconds++;
+    if (seconds === 60) {
+      seconds = 0;
+      minutes++;
+      if (minutes === 60) {
+        minutes = 0;
+        hours++;
+      }
     }
   }
 
   const h = hours < 10 ? "0" + hours : hours;
   const m = minutes < 10 ? "0" + minutes : minutes;
   const s = seconds < 10 ? "0" + seconds : seconds;
+  const mi = milliseconds < 10 ? "0" + milliseconds : milliseconds;
 
-  displayTime.innerHTML = h + ":" + m + ":" + s;
+  displayTime.innerHTML = h + ":" + m + ":" + s + ":" + mi;
 }
 
-let currentStatus = "pause";
+let currentStatus = "Stop";
 
 function runStopwatch() {
-  if (currentStatus === "play") {
-    currentStatus = "pause";
-    playPauseBtn.innerHTML = "Play";
+  if (currentStatus === "Start") {
+    currentStatus = "Stop";
+    playPauseBtn.innerHTML = "Start";
 
     clearInterval(timer);
   } else {
-    currentStatus = "play";
-    playPauseBtn.innerHTML = "Pause";
+    currentStatus = "Start";
+    playPauseBtn.innerHTML = "Stop";
 
     if (timer !== null) {
       clearInterval(timer);
     }
-    timer = setInterval(stopwatch, 1000);
+    timer = setInterval(stopwatch, 10);
   }
 }
 
@@ -46,9 +51,9 @@ function stopStopwatch() {
 }
 
 function resetStopwatch() {
-  [seconds, minutes, hours] = [0, 0, 0];
+  [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
   clearInterval(timer);
-  displayTime.innerHTML = "00:00:00";
-  playPauseBtn.innerHTML = "Play";
-  currentStatus = "pause";
+  displayTime.innerHTML = "00:00:00:00";
+  playPauseBtn.innerHTML = "Start";
+  currentStatus = "Stop";
 }
